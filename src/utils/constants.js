170c10e3baa4c12fa9d2d023e4f5620e9b491eb9
RashmiSubramani <rashmisubramani15@gmail.com>
2025-10-16 14:39:40 +0530

    refactor: updated api key in env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY = "AIzaSyAxxMqEBAyz_livnDGiX3p4xXhqbm7NfOA";
+export const GOOGLE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export const YOUTUBE_SEARCH_API = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
 export const YOUTUBE_COMMENTS_API = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&key=${GOOGLE_API_KEY}`;
